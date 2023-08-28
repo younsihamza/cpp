f2a2ae56b3f8ae0b1a3e8fa6bb4b3f144f9d5921
Hamza Younsi <hyounsi@e3r6p2.1337.ma>
2023-08-28 20:59:09 +0100

    cpp

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -12,8 +12,34 @@ Intern& Intern::operator=(const Intern& main)
 Intern::~Intern()
 {}
 
-Form* makeForm(const std::string& FormName ,const std::string& target)
+Form* Intern::makeForm(const std::string& FormName ,const std::string& target)
 {
+    int i;
     std::string NameForms[3] = {"ShrubberyCreationForm","RobotomyRequestForm","PresidentialPardonForm"};
-    Form* forms[3] = {};
+    Form* (Intern::*fp[3])(const std::string&) = {&Intern::cloneShrubbery,&Intern::cloneRoboto,&Intern::clonePresidential};
+    for(i = 0 ; i < 3 ;i++)
+    {
+        if(FormName == NameForms[i])
+        {
+            std::cout << "Intern creates " <<  FormName <<std::endl;
+            return  (this->*fp[i])(target);
+        }
+    }
+    std::cout << FormName << ": not found !" <<std::endl;
+}
+
+
+Form* Intern::cloneShrubbery(const std::string& target)
+{
+    return (new ShrubberyCreationForm(target));
+}
+
+Form* Intern::cloneRoboto(const std::string& target)
+{
+    return (new RobotomyRequestForm(target));
+}
+
+Form* Intern::clonePresidential(const std::string& target)
+{
+    return (new PresidentialPardonForm(target));
 }
--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -1,6 +1,9 @@
 #ifndef INTERN_HPP
 #define INTERN_HPP
 #include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 class Intern
 {   
     public:
@@ -8,6 +11,10 @@ class Intern
         Intern(const Intern& main);
         Intern& operator=(const Intern& main);
         ~Intern();
+
+        Form *cloneShrubbery(const std::string& target);
+        Form *cloneRoboto(const std::string& target);
+        Form *clonePresidential(const std::string& target);
 
         Form* makeForm(const std::string& FormName ,const std::string& target);
 };
